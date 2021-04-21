4415b1420a793c6e9daee770aa218f069fa640c6
mitch-mooney <mitchellgmooney@gmail.com>
2021-04-21 17:32:25 +1000

    add glicko table back

--- source_code/Store_predictions.R
+++ source_code/Store_predictions.R
@@ -119,7 +119,7 @@ score_sim$score_sim %>%
   mutate(result = ifelse(value < 0, opp, team)) %>% 
   filter(game < (games/2 +1)) %>% #change this to suit how many matches there are that round
   ggplot(aes(x = value, color = team))+
-  stat_ecdf(size = 1.5)+
+  stat_ecdf(size = 1.25)+
   geom_vline(xintercept = 0, size = 1.5, alpha = 0.9, color = '#498181')+
   geom_hline(yintercept = 0.5, size = 1.5,alpha = 0.9, color = '#498181')+
   scale_colour_manual(values = cols)+
@@ -128,7 +128,46 @@ score_sim$score_sim %>%
        y = "cummulative density")+
   xlim(-75,75) +
   # Add images to end point of line graph per team
-  theme_AFL(base_size = 10, background_hex = '#64B2B2')+
+  theme_AFL(base_size = 8, background_hex = '#64B2B2')+
   theme(legend.position = 'None')+
   facet_grid(~game+match,labeller = label_wrap_gen(width = 0.5, multi_line = TRUE))
 
+matches<-results %>% 
+  filter(Season >= 2010) %>% 
+  group_by(Season, Round.Number) %>% 
+  summarise_each(funs(n_distinct(Date))) %>% 
+  select(Season, Round.Number, Date)
+
+lag <-tail(matches$Date, 1)
+round_num <- tail(matches$Round.Number, 1)
+
+#glicko ratings table
+rating_history <- glicko %>% 
+  filter(var == "Rating") %>% 
+  group_by(match) %>% 
+  mutate(rank = rank(-value))%>% 
+  group_by(Team) %>% 
+  mutate(lag = lag(rank, n =lag)) %>% 
+  ungroup()%>% 
+  mutate(change = ifelse(rank < lag, "Up", ifelse(rank > lag, "Down", "Unchanged")),
+         match = as.numeric(match)) %>% 
+  filter(match == max(match)) %>% 
+  select(Team, change) %>%
+  rename(Player = Team)
+
+team_rate <- glicko_rate$ratings
+team_rate %<>% 
+  select(!c(Lag, Deviation, Volatility)) %>% 
+  mutate(Rating = round(Rating, 0), Rank = rank(-Rating)) %>% 
+  select(Rank, Player, Rating)
+
+team_rate<-left_join(rating_history, team_rate, by = c("Player"))
+
+spark_table <- glicko_clean %>%
+  group_by(Team) %>%
+  summarise(Rating = round(tail(value, n = 1), 0),sparkline = list(tail(value, n = round_num))) %>% 
+  rename(Player = Team)
+
+spark_table <- merge(team_rate,spark_table, by=c("Rating", "Player"), all.x=TRUE, all.y=TRUE)
+
+reactable_function(data = spark_table)
