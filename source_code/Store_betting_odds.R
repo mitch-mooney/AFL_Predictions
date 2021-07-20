fadb0574f5cbff83a8e5638f6539299b2c1643d4
mitch-mooney <mitchellgmooney@gmail.com>
2021-07-21 08:57:02 +1000

    Round 19 commit 1

--- source_code/Store_betting_odds.R
+++ source_code/Store_betting_odds.R
@@ -2,7 +2,7 @@
 results<-fetch_results_afltables(season = 2021)
 
 newest.results <- results %>% 
-  filter(Season == 2021, Round.Number == 17) %>% 
+  filter(Season == 2021, Round.Number == 18) %>% 
   select(Date, Venue, Round.Number, Home.Team, Away.Team, Home.Points, Away.Points)
 
 newest.betting <- betting_join %>% 
