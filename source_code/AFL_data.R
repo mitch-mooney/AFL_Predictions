393ae286b7b2ac1d0254cd5bdd13be8302dff3a3
mitch-mooney <mitchellgmooney@gmail.com>
2022-03-31 13:01:21 +1100

    2022 Round 3 Commit 2 and Keras updated code

--- source_code/AFL_data.R
+++ source_code/AFL_data.R
@@ -8,7 +8,7 @@ library(ggpmisc)
 library(magrittr)
 
 # Get Football Draw
-fixture <- fetch_fixture_squiggle(season = 2022, round_number = 2)
+fixture <- fetch_fixture_squiggle(season = 2022, round_number = 3)
 fixture %<>%
   rename(Date = date,
          Season = year,
@@ -189,7 +189,7 @@ match <- dplyr::inner_join(match, bet, by=c("Date","Status", "Team"))
 ##########----- Add next round fixture to dataframe -----########## 
 
 # add new fixture to dataframe for prediction
-round <- wrangle_fixture(round = 2)
+round <- wrangle_fixture(round = 3)
 #round <- readr::read_csv('csv_files/fixture.csv')
 # change date format
 round$Date<- as.Date(round$Date,format = "%Y-%m-%d %H:%M:%S")
--- source_code/Store_betting_odds.R
+++ source_code/Store_betting_odds.R
@@ -2,7 +2,7 @@
 results<-fetch_results_afltables(season = 2022)
 
 newest.results <- results %>% 
-  filter(Season == 2022, Round.Number == 1) %>% 
+  filter(Season == 2022, Round.Number == 2) %>% 
   select(Date, Venue, Round.Number, Home.Team, Away.Team, Home.Points, Away.Points)
 
 newest.betting <- betting_join %>% 
