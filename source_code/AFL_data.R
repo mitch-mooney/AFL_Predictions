b0bf93631b94a7c2339558ee7e01aff27f8494d8
mitch-mooney <mitchellgmooney@gmail.com>
2022-05-06 16:51:11 +1000

    2022 Round 8 Commit 2

--- source_code/AFL_data.R
+++ source_code/AFL_data.R
@@ -7,7 +7,7 @@ library(reshape2)
 library(ggpmisc)
 library(magrittr)
 
-round.no <- 6
+round.no <- 8
 
 # Get Football Draw
 fixture <- fetch_fixture_squiggle(season = 2022, round_number = round.no)
