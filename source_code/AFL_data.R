7c270dd308a03a6cd73ec9ff60af1c3bd8094295
mitch-mooney <mitchellgmooney@gmail.com>
2022-09-23 10:13:54 +1000

    2022 GF commit 1

--- source_code/AFL_data.R
+++ source_code/AFL_data.R
@@ -7,7 +7,7 @@ library(reshape2)
 library(ggpmisc)
 library(magrittr)
 
-round.no <-26
+round.no <-27
 fixture <- fetch_fixture_squiggle(season = 2022, round_number = round.no)
 fixture %<>%
   rename(Date = date,
