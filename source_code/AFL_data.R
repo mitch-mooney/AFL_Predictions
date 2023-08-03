387e5150de4ea7899f67fe0c33b8ff02647a0b6a
mitch-mooney <mitchellgmooney@gmail.com>
2023-08-03 14:11:03 +1000

    Round  21 2023 Commit 1

--- source_code/AFL_data.R
+++ source_code/AFL_data.R
@@ -7,7 +7,7 @@ library(reshape2)
 library(ggpmisc)
 library(magrittr)
 
-round.no <- 20
+round.no <- 21
 YEAR <- as.numeric(format(Sys.Date(), "%Y"))
 fixture <- fetch_fixture_squiggle(season = YEAR, round_number = round.no)
 fixture %<>%
