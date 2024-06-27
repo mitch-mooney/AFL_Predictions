1d76b059c4c3a95e540e68f4b5d6097df73e7420
mitch-mooney <mitchellgmooney@gmail.com>
2024-06-28 09:48:24 +1000

    2024 Round 16 commit 1

--- source_code/AFL_data.R
+++ source_code/AFL_data.R
@@ -7,7 +7,7 @@ library(reshape2)
 library(ggpmisc)
 library(magrittr)
 
-round.no <-15
+round.no <-16
 YEAR <- as.numeric(format(Sys.Date(), "%Y"))
 fixture <- fetch_fixture_squiggle(season = YEAR, round_number = round.no)
 fixture %<>%
