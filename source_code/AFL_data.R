655beb6e75294cbe51d0f218d3f89356aa45542d
mitch-mooney <mitchellgmooney@gmail.com>
2025-06-02 09:43:40 +1000

    2025 Round 13

--- source_code/AFL_data.R
+++ source_code/AFL_data.R
@@ -7,7 +7,7 @@ library(reshape2)
 library(ggpmisc)
 library(magrittr)
 
-round.no <- 12
+round.no <- 13
 YEAR <- as.numeric(format(Sys.Date(), "%Y"))
 fixture <- fetch_fixture_squiggle(season = YEAR, round_number = round.no)
 fixture %<>%
